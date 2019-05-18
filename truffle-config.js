c7e28e205a3c0be8bd93fcb7f6fc4bd1b95d8c0a
Brendan Asselstine <mail.asselstine@gmail.com>
2019-05-18 14:37:31 -0400

    Deployed to Rinkeby

--- truffle-config.js
+++ truffle-config.js
@@ -19,8 +19,8 @@ module.exports = {
         0, // we start with address[0]
         8 // notice that we unlock eight: which will be address[0] and address[1]
       ),
-      network_id: 3,
-      gas: 8000000,
+      network_id: '*',
+      gas: 6700000,
       gasPrice: 10 * 1000000000
     }
   },
@@ -31,12 +31,12 @@ module.exports = {
     }
   },
 
-  solc: {
-    optimizer: {
-      enabled: true,
-      runs: 1
-    }
-  },
+  // solc: {
+  //   optimizer: {
+  //     enabled: true,
+  //     runs: 1
+  //   }
+  // },
 
   mocha: {
     reporter: 'eth-gas-reporter',
